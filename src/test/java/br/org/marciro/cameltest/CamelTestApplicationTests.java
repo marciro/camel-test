4b81022829ad5bd87dbf44fd48c5410dfef65a8a
Marcelo Cícero <marciro@gmail.com>
2023-04-03 20:36:29 -0300

    Commit inicial

---
+++ src/test/java/br/org/marciro/cameltest/CamelTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.marciro.cameltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
